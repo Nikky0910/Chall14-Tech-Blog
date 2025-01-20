b59b5b418c1291089d8854376682d35bae181f81
Marianna <mariannaniko1092@gmail.com>
2025-01-19 21:23:56 -0500

    Create editPost.js

---
+++ public/js/editPost.js
@@ -0,0 +1,27 @@
+const editButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    const name = document.getElementById('name-edit').value
+    const description = document.getElementById('description-edit').value
+
+    if (name && description) {
+      const response = await fetch(`/api/projects/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/view-posts');
+      } else {
+        alert('Failed to update project');
+      }
+    }
+  }
+};
+
+document
+  .querySelector('.edit-form')
+  .addEventListener('click', editButtonHandler);
