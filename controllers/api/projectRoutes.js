a05312cad06c3f0ff5661a18e115f8dff0091927
Marianna <mariannaniko1092@gmail.com>
2025-01-19 21:22:33 -0500

    Add route to handle update requests in projectRoutes.js

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -35,4 +35,29 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
+// http://localhost:3001/api/projects/13
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.update(
+      {
+      name: req.body.name,
+      description: req.body.description
+      }, 
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
